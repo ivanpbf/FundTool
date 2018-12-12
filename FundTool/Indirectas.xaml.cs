4a415bda48cca37c32beb4d059a0b85971c9ca0a
ivanpbf <ivan27rlb@gmail.com>
2018-12-11 21:18:41 -0430

    agregado algunas cosas de indirecta

--- FundTool/Indirectas.xaml.cs
+++ FundTool/Indirectas.xaml.cs
@@ -19,9 +19,62 @@ namespace FundTool
     /// </summary>
     public partial class Indirectas : Window
     {
+        public String tipoDeSuelo;
         public Indirectas()
         {
             InitializeComponent();
+            this.GranularGrid.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void CancelarSuelo(object sender, RoutedEventArgs e)
+        {
+            this.Close(); //cierra la ventana
+        }
+
+        private void NumericOnly(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new System.Text.RegularExpressions.Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void AceptarSuelo(object sender, RoutedEventArgs e)
+        {
+            if (!(Boolean)this.Granular.IsChecked && !(Boolean)this.Cohesivo.IsChecked && !(Boolean)this.GranularCohesivo.IsChecked)
+            {
+                MessageBox.Show("Por favor elija un tipo de suelo.");
+                return;
+            }
+            else if ((Boolean)this.Granular.IsChecked)
+            {
+                this.tipoDeSuelo = "Granular";
+                this.GranularGrid.Visibility = Visibility.Visible;
+                this.Granular.IsEnabled = false;
+                this.Cohesivo.IsEnabled = false;
+                this.GranularCohesivo.IsEnabled = false;
+                this.Siguientes.IsEnabled = false;
+                return;
+            }
+            else if ((Boolean)this.Cohesivo.IsChecked)
+            {
+                this.tipoDeSuelo = "Cohesivo";
+                this.CohesivoGrid.Visibility = Visibility.Visible;
+                this.Granular.IsEnabled = false;
+                this.Cohesivo.IsEnabled = false;
+                this.GranularCohesivo.IsEnabled = false;
+                this.Siguientes.IsEnabled = false;
+                return;
+            }
+            else if ((Boolean)this.GranularCohesivo.IsChecked)
+            {
+                this.tipoDeSuelo = "GranularCohesivo";
+                this.GranularCohesivoGrid.Visibility = Visibility.Visible;
+                this.Granular.IsEnabled = false;
+                this.Cohesivo.IsEnabled = false;
+                this.GranularCohesivo.IsEnabled = false;
+                this.Siguientes.IsEnabled = false;
+                return;
+            }
         }
     }
+
 }
